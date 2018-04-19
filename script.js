4147446c2c9dde8ad6f09057ede85c25a21d2707
Trelanor <jonathan.n@codeur.online>
2018-04-19 14:27:28 +0200

    update

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 
-var hamburgerElement = document.getElementById("hamburger-img");
+var hamburgerElement = document.getElementById("hamburger");
 var menuElement = document.getElementById("menu");
  
-hamburgerElement.addEventListener('click', function() {
+  hamburgerElement.addEventListener('click', function() {
+    
+    if(menuElement.style.display == "block")
+      menuElement.style.display = "none";
+    else
+      menuElement.style.display = "block";
+    
+  })
   
-  if(menuElement.style.display == "block")
-    menuElement.style.display = "none";
-  else
-    menuElement.style.display = "block";
-  
-})
-  
-
 
 
